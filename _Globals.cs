002041e9ad721aa90df2e581564e23fe412079c0
Rartrin <polliwhirl@gmail.com>
2016-08-30 13:41:54 -0700

    Fixed saving error

--- _Globals.cs
+++ _Globals.cs
@@ -6,38 +6,44 @@ using Terraria.ModLoader;
 
 namespace ModularWeaponry
 {
-    public class GItem : GlobalItem
-    {
-        public override bool NeedsCustomSaving(Item item)
-        {
-            IInfo info = item.GetModInfo<IInfo>(mod);
-            return info.modules != null;
-        }
+	public class GItem : GlobalItem
+	{
+		public override bool NeedsCustomSaving(Item item)
+		{
+			IInfo info = item.GetModInfo<IInfo>(mod);
+			return info.modules != null;
+		}
 
-        public override void SaveCustomData(Item item,BinaryWriter writer)
-        {
-            IInfo info=item.GetModInfo<IInfo>(mod);
-            string writeString="";
-            for(byte i=0;i<info.modules.Length;++i)
-            {
-                writeString+=ItemLoader.GetItem(info.modules[i]).item.name+";";
-            }
-            writer.Write(writeString);
-        }
-        public override void LoadCustomData(Item item, BinaryReader reader)
-        {
-            IInfo info = item.GetModInfo<IInfo>(mod);
-            info.modules = new ushort[5];
-            string[] splitModules = reader.ReadString().Split(';');
-            for (byte i=0;i<info.modules.Length;++i)
-            {
-                info.modules[i]=(ushort)mod.ItemType(splitModules[i]);
-            }
-        }
-    }
+		public override void SaveCustomData(Item item,BinaryWriter writer)
+		{
+			IInfo info=item.GetModInfo<IInfo>(mod);
+			string writeString="";
+			for(byte i=0;i<info.modules.Length;++i)
+			{
+				{
+					writeString+=ItemLoader.GetItem(info.modules[i]).item.name+";";
+				}
+				else
+				{
+					writeString+=" ;";
+				}
+			}
+			writer.Write(writeString);
+		}
+		public override void LoadCustomData(Item item, BinaryReader reader)
+		{
+			IInfo info = item.GetModInfo<IInfo>(mod);
+			info.modules = new ushort[5];
+			string[] splitModules = reader.ReadString().Split(';');
+			for (byte i=0;i<info.modules.Length;++i)
+			{
+				info.modules[i]=(ushort)mod.ItemType(splitModules[i]);
+			}
+		}
+	}
 
-    public class IInfo : ItemInfo
-    {
-        public ushort[] modules;
-    }
+	public class IInfo : ItemInfo
+	{
+		public ushort[] modules;
+	}
 }
